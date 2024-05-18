9714c1739990c3127f23cd85bcf0584c3dcde8dc
yuanxichennn <yuanxi_chen@126.com>
2024-05-18 15:48:15 +0800

    Updated login, register, post and comments page style to match main theme with pixel style font and consistent design

--- static/front/js/register.js
+++ static/front/js/register.js
@@ -2,18 +2,18 @@ function captchaBtnClickEvent(event) {
   event.preventDefault();
   var $this = $(this);
 
-  // 获取邮箱
+  // Obtain the email
   var email = $("input[name='email']").val();
   var reg = /^\w+((.\w+)|(-\w+))@[A-Za-z0-9]+((.|-)[A-Za-z0-9]+).[A-Za-z0-9]+$/;
   if (!email || !reg.test(email)) {
-    alert("请输入正确格式的邮箱！");
+    alert("Please enter the correct format email!");
     return;
   }
 
   zlajax.get({
     url: "/user/mail/captcha?mail=" + email
   }).done(function (result) {
-    alert("验证码发送成功！");
+    alert("Verification code sent successfully!");
   }).fail(function (error) {
     alert(error.message);
   })
@@ -22,13 +22,13 @@ function captchaBtnClickEvent(event) {
 $(function () {
   $('#captcha-btn').on("click",function(event) {
     event.preventDefault();
-    // 获取邮箱
+    // Obtain the email address
     var email = $("input[name='email']").val();
 
     zlajax.get({
       url: "/user/mail/captcha?mail=" + email
     }).done(function (result) {
-      alert("验证码发送成功！");
+      alert("Verification code sent successfully!");
     }).fail(function (error) {
       alert(error.message);
     })
